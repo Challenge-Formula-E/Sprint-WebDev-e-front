4d27446c8526db7ae02bfe0b7d5549b2da02638a
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2024-09-27 15:18:32 -0300

    header upgrade

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,18 +1,17 @@
 import logo from '../assets/logo.png';
 import '../styles/header.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
     return (
-      <div className="header">
-        <div className="rectangle-2"></div>
+      <div className="header rectangle-2">
         <img className="logo" src={logo} alt="Logo" />
         <nav className="nav-items">
-          <Link to='/' className="nav-item home" >Home</Link>
-          <Link to='/races' className="nav-item races">Races</Link>
-          <Link to='/about' className="nav-item about">About</Link>
-          <Link to='/coins' className="nav-item coins">E-Coins</Link>
-          <Link to='/news' className="nav-item news">News</Link>
+          <NavLink to='/' className="nav-item home" >Home</NavLink>
+          <NavLink to='/races' className="nav-item races">Races</NavLink>
+          <NavLink to='/about' className="nav-item about">About</NavLink>
+          <NavLink to='/coins' className="nav-item coins">E-Coins</NavLink>
+          <NavLink to='/news' className="nav-item news">News</NavLink>
         </nav>
       </div>
 
