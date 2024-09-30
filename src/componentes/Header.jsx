33d05e488f0f4fa2b05809721c7f7aa7e1757527
Enzo Miletta <milettaaa@gmail.com>
2024-09-30 20:22:43 -0300

    end commit?
    About, news, css, photos, header inseridos

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,20 +1,34 @@
 import logo from '../assets/logo.png';
 import '../styles/header.css';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 export const Header = () => {
-    return (
-      <div className="header rectangle-2">
-        <img className="logo" src={logo} alt="Logo" />
-        <nav className="nav-items">
-          <NavLink to='/' className="nav-item home" >Home</NavLink>
-          <NavLink to='races' className="nav-item races">Races</NavLink>
-          <NavLink to='about' className="nav-item about">About</NavLink>
-          <NavLink to='coins' className="nav-item coins">E-Coins</NavLink>
-          <NavLink to='news' className="nav-item news">News</NavLink>
-        </nav>
+  const [isOpen, setIsOpen] = useState(false); // Estado para controlar o menu
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Alterna entre aberto e fechado
+  };
+
+  return (
+    <div className="header rectangle-2">
+      <img className="logo" src={logo} alt="Logo" />
+      
+      {/* Ícone do Menu Hamburguer */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
 
-    );
-  };
-  
+      {/* Menu de navegação */}
+      <nav className={`nav-items ${isOpen ? 'open' : ''}`}>
+        <NavLink to='/' className="nav-item home" onClick={toggleMenu}>Home</NavLink>
+        <NavLink to='races' className="nav-item races" onClick={toggleMenu}>Races</NavLink>
+        <NavLink to='about' className="nav-item about" onClick={toggleMenu}>About</NavLink>
+        <NavLink to='coins' className="nav-item coins" onClick={toggleMenu}>E-Coins</NavLink>
+        <NavLink to='news' className="nav-item news" onClick={toggleMenu}>News</NavLink>
+      </nav>
+    </div>
+  );
+};
