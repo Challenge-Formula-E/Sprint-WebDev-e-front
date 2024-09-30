2ba4306eef2eacb6ae6c0890a498776475c51040
Matheus-V-Alves <mviniciussouzaalves@gmail.com>
2024-09-30 02:57:31 -0300

    Página de E-coins, parte do profile

--- src/pages/Coins.jsx
+++ src/pages/Coins.jsx
@@ -1,32 +1,74 @@
 import '../styles/coins.css'
+import user from "../assets/user.png"
 
 function Coins() {
   return (
     <div className="profile-container">
-      <header className="profile-header">
-        <div className="user-info">
-          <div className="user-icon"></div>
-          <h1>HELLO, STEVE!</h1>
-          <button className="leave-button">leave</button>
-        </div>
-      </header>
-      <div className="social-media">
-        <div className="social-item">
-          <span className="social-icon tiktok" src="../assets/tiktok.png"></span>
-          <span className="social-email">tiktokuser@email123</span>
-          <span className="social-posts">07 posts</span>
-        </div>
-        <div className="social-item">
-          <span className="social-icon facebook"></span>
-          <span className="social-email">Facebookuser@email.123</span>
-          <span className="social-posts">01 posts</span>
-        </div>
-        <div className="social-item">
-          <span className="social-icon instagram"></span>
-          <span className="social-email">Instagramuser@email.123</span>
-          <span className="social-posts">93 posts</span>
+      <div className='back-profile'>
+        <header className="profile-header">
+          <div className='user-info'>
+            <div className="user-icon" style={{backgroundImage: `url(${user})`}}></div>
+            <h1 className='user-name'>HELLO, STEVE!</h1>
+          </div>
+            <button className="leave-button">leave</button>
+          
+        </header>
+
+
+        <div className="social-media">
+
+
+          <div className="social-item">
+            <div className='social-plataform'>
+              <span className="social-icon tiktok" src="../assets/tiktok.png"></span>
+              <div className='conect'>
+                <span className="social-email">tiktokuser@email123</span>
+                <p className='social-connected'>CONNECTED</p>
+              </div>
+            </div>
+
+            <span className="social-posts">07 posts</span>
+          </div>
+
+
+          <div className="social-item">
+            <div className='social-plataform'>
+              <span className="social-icon facebook"></span>
+              <div className='conect'>
+                <span className="social-email">Facebookuser@email.123</span>
+                <p className='social-connected'>CONNECTED</p>
+              </div>
+            </div>
+            <span className="social-posts">01 posts</span>
+          </div>
+
+
+          <div className="social-item">
+            <div className='social-plataform'>
+              <span className="social-icon instagram"></span>
+              <div className='conect'>
+                <span className="social-email">Instagramuser@email.123</span>
+                <p className='social-connected'>CONNECTED</p>
+              </div>
+            </div>
+            <span className="social-posts">13 posts</span>
+          </div>
+
+
+          <div className="social-item">
+            <div className='social-plataform'>
+              <span className="social-icon twitter"></span>
+              <div className='conect'>
+                <span className="social-email">Twitteruser@email.123</span>
+                <p className='social-connected'>CONNECTED</p>
+              </div>
+            </div>
+            <span className="social-posts">13 posts</span>
+          </div>
+
         </div>
       </div>
+
       <div className="stats">
         <div className="e-coins">
           <div className="coin-icon">E</div>
