0e43db79d6c46e0a1a9c10adc10521e4ade3f41c
Matheus Zanutin <119885089+Mats057@users.noreply.github.com>
2024-09-30 21:50:59 -0300

    responsividade

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
 import '../styles/about.css';
 import carros from '../assets/carros.png';
 import carro from '../assets/carromahindra.png';
-import capa from '../assets/mahindracar.png';
 import logo from '../assets/logo.png';
-import fundo from '../assets/fundomoedas.png'
-import { BiFontSize } from 'react-icons/bi';
 
 function About () {
   return (
@@ -18,12 +14,12 @@ function About () {
 
 
 
-        <h2 className='titulo'>About</h2>
+        <h2 className='titulo h2-about'>About</h2>
 
       <section className="about-section">
 
-        <p > 
-          Formula E is the world's first all-electric international single-seater championship.
+        <p className='p-about'> 
+          Formula E is the world&apos;s first all-electric international single-seater championship.
           <br />
           Officially known as the ABB FIA Formula E World Championship,
           <br /> it encourages the development 
@@ -36,18 +32,18 @@ function About () {
 
 
 
-        <h2 className='benefits'>Benefits</h2>
+        <h2 className='benefits h2-about'>Benefits</h2>
 
 
 
       <section className="benefits-section">
 
-        <p>
+        <p className='p-about'>
           The end goal is to promote sustainable mobility as part of efforts to combat urban <br /> air pollution 
           and lessen the effects of climate change. 
           <br />
-          According to the Championship’s organizers, “Formula E is more than just a racing series <br /> 
-          – it’s 
+          According to the Championship&apos;s organizers, “Formula E is more than just a racing series <br /> 
+          – it&apos;s 
           a battle for the future.
           <br /> Our radical cars – all powered by electricity – pave the way for the road cars 
           of tomorrow, <br /> with the series acting as a competitive platform to test and develop the latest in electric technology.”
@@ -64,7 +60,7 @@ function About () {
       </div>
 
       <section className="history-section">
-        <p>
+        <p className='p-about'>
           Since making its debut in the grounds of the Olympic Park in Beijing in 2014, Formula E has grown <br />
           into a global entertainment brand, attracting a grid full of the best racing drivers and teams around. <br />
           Now in its 9th season, Formula E features 22 drivers divided among 11 teams representing marques including 
